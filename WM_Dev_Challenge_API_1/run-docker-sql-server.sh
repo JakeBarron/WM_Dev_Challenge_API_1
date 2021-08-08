7c85e8b84742acdc60f9f35232f9b56fa6d84cc3
Jake Barron <jake.barron@turner.com>
2021-08-08 18:00:25 -0400

    * README.MD: * appsettings.json: * run-docker-sql-server.sh:
    * Titles.bak: add readme change credentials

---
+++ WM_Dev_Challenge_API_1/run-docker-sql-server.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+sudo docker pull mcr.microsoft.com/mssql/server:2019-latest
+
+sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=YourStrong@Passw0rd" \
+   -p 1433:1433 --name sqlserver -h sqlserverhost \
+   -d mcr.microsoft.com/mssql/server:2019-latest
+
+docker cp ./Titles.bak sqlserver:/Titles.bak
