b26c9ba95844844673234e18f8c05e5b912c0964
Jake Barron <jake.barron@turner.com>
2021-08-07 21:16:18 -0400

    Add "search" function and details by id

--- WM_Dev_Challenge_API_1/Controllers/TitlesController.cs
+++ WM_Dev_Challenge_API_1/Controllers/TitlesController.cs
@@ -2,9 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace WM_Dev_Challenge_API_1.Controllers
 {
@@ -12,5 +11,126 @@ namespace WM_Dev_Challenge_API_1.Controllers
     [ApiController]
     public class TitlesController : ControllerBase
     {
+        private readonly TitlesContext _context;
+
+        public TitlesController(TitlesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Titles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Title>>> GetTitles()
+        {
+            return await _context.Titles.ToListAsync();
+        }
+
+        // GET: api/Titles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Title>> GetTitle(int id)
+        {
+            var title = await _context.Titles
+                .Include(t => t.Awards)
+                .Include(t => t.StoryLines)
+                .Include(t => t.TitleGenres)
+                .Include(t => t.OtherNames)
+                .FirstOrDefaultAsync(t => t.TitleId == id);
+
+            if (title == null)
+            {
+                return NotFound();
+            }
+
+            return title;
+        }
+
+
+        [HttpGet("search/{searchTerm}")]
+        public async Task<ActionResult<IEnumerable<Title>>> SearchByTitleName(string searchTerm)
+        {
+            Console.Write(searchTerm);
+            return await _context.Titles.Where(title => title.TitleName.Contains(searchTerm)).ToListAsync();
+        }
+
+
+        // PUT: api/Titles/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTitle(int id, Title title)
+        {
+            if (id != title.TitleId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(title).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TitleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        // POST: api/Titles
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Title>> PostTitle(Title title)
+        {
+            _context.Titles.Add(title);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TitleExists(title.TitleId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTitle", new { id = title.TitleId }, title);
+        }
+
+        // DELETE: api/Titles/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Title>> DeleteTitle(int id)
+        {
+            var title = await _context.Titles.FindAsync(id);
+            if (title == null)
+            {
+                return NotFound();
+            }
+
+            _context.Titles.Remove(title);
+            await _context.SaveChangesAsync();
+
+            return title;
+        }
+
+        private bool TitleExists(int id)
+        {
+            return _context.Titles.Any(e => e.TitleId == id);
+        }
     }
 }
